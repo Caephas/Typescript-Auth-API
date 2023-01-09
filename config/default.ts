fac4b36e3798d355ee55870b1567b68173f851c5
Caephas <arinzeobidiegwu@gmail.com>
2023-01-09 16:07:57 +0100

    feat: User creation implemented

--- config/default.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    //change it in postman collection
-    port: 1337,
-    dbUri: "mongodb+srv://Caephas:Ephata35923@cluster0.9zwl4vw.mongodb.net/?retryWrites=true&w=majority",
-    saltWorkFactor: 10
-}
