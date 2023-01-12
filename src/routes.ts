a71c6aaa22acb051e886145c25bec5b54ceb5c33
Caephas <arinzeobidiegwu@gmail.com>
2023-01-12 13:39:35 +0100

    feat: added session functionality

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,9 @@ import { Express, Request, Response } from "express"
 import { createUserHandler } from "./controller/user.controller"
 import validate from "./middleware/validateResource"
 import { createUserSchema } from './schema/user.schema';
+import { createSessionHandler,deleteSessionHandler,getUserSessionsHandler } from "./controller/session.controller";
+import { createSessionSchema } from "./schema/session.schema";
+import requireUser from "./middleware/requireUser";
 
 
 function routes(app: Express) { 
@@ -10,6 +13,12 @@ function routes(app: Express) {
     })
 
     app.post('/api/users', validate(createUserSchema), createUserHandler)
+
+    app.post('/api/sessions', validate(createSessionSchema), createSessionHandler)
+
+    app.get('/api/sessions', requireUser, getUserSessionsHandler)
+
+    app.delete('/api/sessions', requireUser,deleteSessionHandler)
 }
 
 export default routes
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,5 +1,5 @@
 import {omit} from 'lodash'
-import { DocumentDefinition } from "mongoose";
+import { DocumentDefinition, FilterQuery } from "mongoose";
 import UserModel, { UserDocument } from "../models/user.model";
 
 export async function createUser(input: DocumentDefinition<Omit<UserDocument, "createdAt" | "updatedAt" | "comparePassword">>) {
@@ -9,4 +9,28 @@ export async function createUser(input: DocumentDefinition<Omit<UserDocument, "c
     } catch (e: any) {
         throw new Error(e)
     }
+}
+
+export async function validatePassword({
+    email, password
+}: {
+        email: string
+        password: string
+    }) {
+    const user = await UserModel.findOne({ email })
+    
+    if (!user) {
+        return false
+    }
+    const isValid = await user.comparePassword(password)
+
+    if (!isValid) {
+        return false
+    } else {
+        return user
+    }
+}
+
+export async function findUser(query: FilterQuery<UserDocument>) {
+    return UserModel.findOne(query).lean()
 }
